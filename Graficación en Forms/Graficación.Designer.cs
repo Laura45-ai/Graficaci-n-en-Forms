4f79f1b3c0ef60ce0327b01fcbca55e8abe9a564
laura45-ai <laura.colorado451@pascualbravo.edu.co>
2024-10-22 10:49:41 -0500

    se subió

---
+++ Graficación en Forms/Graficación.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Graficación_en_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.balon = new System.Windows.Forms.PictureBox();
+            this.tiempo = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.balon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // balon
+            // 
+            this.balon.Image = global::Graficación_en_Forms.Properties.Resources.balonnn;
+            this.balon.Location = new System.Drawing.Point(345, 249);
+            this.balon.Name = "balon";
+            this.balon.Size = new System.Drawing.Size(104, 100);
+            this.balon.TabIndex = 0;
+            this.balon.TabStop = false;
+            // 
+            // tiempo
+            // 
+            this.tiempo.Tick += new System.EventHandler(this.tiempo_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 344);
+            this.Controls.Add(this.balon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.balon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox balon;
+        private System.Windows.Forms.Timer tiempo;
+    }
+}
+
---
+++ Graficación en Forms/Graficación.cs
@@ -0,0 +1,63 @@
+﻿//Laura Cristina Colorado Sánchez
+
+using System;
+using System.Windows.Forms;
+
+namespace Graficación_en_Forms
+{
+    public partial class Form1 : Form
+    {
+        private float velocidadY = -15f; 
+        private float gravedad = 0.5f;
+        private float alturaInicial; 
+        private int rebotes = 0; 
+        private int maxRebotes = 8;
+
+        public Form1()
+        {
+            InitializeComponent();
+            alturaInicial = balon.Top; 
+            tiempo.Interval = 20;
+            tiempo.Tick += new EventHandler(tiempo_Tick); 
+            tiempo.Start();
+        }
+
+        private void tiempo_Tick(object sender, EventArgs e)
+        {
+           
+            balon.Top += (int)velocidadY;
+
+            velocidadY += gravedad;
+
+          
+            if (balon.Top >= alturaInicial)
+            {
+                balon.Top = (int)alturaInicial; 
+                rebotes++;
+
+                
+                velocidadY = -velocidadY * 0.7f;
+
+              
+                velocidadY *= 1.2f; 
+
+                if (rebotes >= maxRebotes)
+                {
+                    velocidadY *= 0.7f; 
+                    if (Math.Abs(velocidadY) < 1f) 
+                    {
+                        velocidadY = 0; 
+                        tiempo.Stop();
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
